dde59cbf63f1a52c13008183ddb60bd5335895c4
Kovg <you@Kovgs-MacBook-Pro.local>
2026-06-19 09:59:02 -0400

    login page ajouter et fonctionne

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion Joueurs</title>
+    <title>FIFA 2026</title>
     <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -11,29 +11,17 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="acceuil.php">Gestion Joueurs</a>
+            <a class="navbar-brand" href="index.php">FIFA 2026</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="acceuil.php">Acceuil</a>
+                        <a class="nav-link" href="index.php">Acceuil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="login.php">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="liste_joueurs.php">Liste des Joueurs</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="ajouter_joueur.php">Ajouter Joueur</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="rechercher_joueur.php">Rechercher Joueur</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="modifier_buts.php">Modifier Buts</a>
+                        <a class="nav-link" href="php/login.php">Login</a>
                     </li>
                 </ul>
             </div>
